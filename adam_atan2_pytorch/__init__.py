661aed292abc2d0ce379b0686fdbc34d0c3fc819
lucidrains <lucidrains@gmail.com>
2024-11-22 06:51:05 -0800

    default adopt atan2 for export given experiments im seeing

--- adam_atan2_pytorch/__init__.py
+++ adam_atan2_pytorch/__init__.py
@@ -1,4 +1,5 @@
 from adam_atan2_pytorch.adam_atan2 import AdamAtan2
-from adam_atan2_pytorch.adopt import Adopt
+from adam_atan2_pytorch.adopt_atan2 import AdoptAtan2
 
 Adam = AdamAtan2
+Adopt = AdoptAtan2
